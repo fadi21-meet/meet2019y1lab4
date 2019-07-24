80d8d0ebe167314569891584e5dbefb5e8887cb2
fadi21-meet <fadi21@meet.mit.edu>
2019-07-24 07:32:29 -0400

    Stopping here

---
+++ lab_4.py
@@ -0,0 +1,22 @@
+import turtle
+finn = turtle.clone()
+finn.shape('turtle')
+finn.goto(100,100)
+finn.forward(100)
+finn.left(90)
+finn.forward(100)
+finn.left(90)
+finn.forward(100)
+finn.left(90)
+finn.forward(100)
+jack = turtle.clone()
+jack.shape('triangle')
+jack.forward(100)
+jack.left(120)
+jack.forward(100)
+jack.left(120)
+jack.forward(100)
+
+
+
+turtle.mainloop()
